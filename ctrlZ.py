b2a01161653bcb1b4eddfaf9ef26939becacff87
Ivan Morozov <vanyam.vvo@gmail.com>
2025-04-12 16:18:19 +1000

    implementation of all directions
    Signed-off-by: Ivan Morozov <vanyam.vvo@gmail.com>

--- ctrlZ.py
+++ ctrlZ.py
@@ -33,15 +33,14 @@ while run:
             start_x = start_y = w = h = 0
         if drawing and e.type == pg.MOUSEMOTION:
             disp.fill("black")
-            if len(surfs): disp.blit(surfs[-1], (0, 0))
+            if len(surfs): disp.blit(surf, (0, 0))
             w, h = e.pos[0] - start_x, e.pos[1] - start_y
         if e.type == pg.KEYUP and e.key == pg.K_z and len(surfs) > 0:
             surfs.pop()
-            surf.fill("black")
             print(surfs)
             rerender()
     if drawing:
-        rect = pg.rect.Rect(start_x, start_y, w, h)
+        rect = pg.rect.Rect(start_x + (w if w < 0 else 0), start_y + (h if h < 0 else 0), abs(w), abs(h))
         pg.draw.rect(disp, "#2bb3fc", rect, 5)
         pg.display.flip()
 
